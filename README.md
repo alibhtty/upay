a050259f440e695f1eb40edd59196e9778759879
alibhtty <77213825+alibhtty@users.noreply.github.com>
2022-11-24 18:36:50 +0100

    v.2

--- README.md
+++
@@ -1,2 +0,0 @@
-# upay
-payment
---
+++ assets/js/variables.js
@@ -0,0 +1,40 @@
+var defaultval = {
+	theme: 'teal',
+	header: 'light',
+	header_align: 'center',
+	menu: 'light',
+	menu_icons: 'on',
+	menu_type: 'left',
+	footer: 'light',
+	footer_type: 'center',
+	//menu_animation: "slide_left",
+	site_mode: 'light',
+	footer_menu: 'show',
+	footer_menu_style: 'light'
+};
+
+var charts_primary_color = '#26A69A';
+var charts_icon_color = '#bdbdbd';
+var charts_accent_color = '#F06292';
+
+var charts_color_1 = '#26C6DA';
+var charts_color_2 = '#4DB6AC';
+var charts_color_3 = '#81C784';
+var charts_color_4 = '#009688';
+var charts_color_5 = '#00BCD4';
+var charts_color_6 = '#80CBC4';
+
+var charts_xaxis_split_line_color = '#ffffff';
+var charts_yaxis_split_line_color = '#ffffff';
+var charts_yaxis_line_color = '#ff0000';
+var charts_timeline_border_color = '#eaeaea';
+
+var charts_series_title_color = '#333333';
+var charts_series_border_color = '#cccccc';
+
+var charts_transparent_color = 'rgba(0,0,0,0)';
+var charts_tooltip_line_color = 'rgba(0,0,0,0.5)';
+var charts_tooltip_shadow_color = 'rgba(0,0,0,.1)';
+
+var charts_series_area_color = 'rgba(63,81,181,0)';
+var charts_series_pointer_color = 'rgba(155, 155, 155, 0.8)';
